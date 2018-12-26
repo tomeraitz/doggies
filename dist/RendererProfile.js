1e64c9d316d6390e1d4e18f6e5053ee208b96bd2
Aviran Levi <aveloofficial@gmail.com>
2018-12-26 20:10:17 +0200

    fixing something

--- dist/RendererProfile.js
+++
@@ -1,9 +0,0 @@
-class RendererProfile {
-    renderProfile(data) {
-        $("#profile-pic-name").empty();
-        let source = $("#profile-template").html();
-        let template = Handlebars.compile(source);
-        let newHTML = template(data);
-        $("#profile-pic-name").append(newHTML);
-    }
-}
