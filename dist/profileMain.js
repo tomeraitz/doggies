dae696e83bbc591f71ddd497f3d088ea07b67400
Aviran Levi <aveloofficial@gmail.com>
2018-12-27 03:42:32 +0200

    add community name to calendar

--- dist/profileMain.js
+++ dist/profileMain.js
@@ -1,18 +1,16 @@
 const renderer = new RendererProfile()
 const profileManager = new ProfileManager()
 
-window.onload = async function() {
-   const user = await profileManager.getUserDetilas(profileManager.UserId)
-   console.log(user)
-   renderer.renderProfile(user)
- }
+window.onload = async function () {
+    const user = await profileManager.getUserDetilas(profileManager.UserId)
+    console.log(user)
+    renderer.renderProfile(user)
+}
 
-$("#upload-btn").on("click", function () {
+$("#profile-image").on("click", function () {
     uploadImage.uploadProfileImage()
 })
 
-$("body").on("click" ,"#logo", function(){
+$("body").on("click", "#logo", function () {
     window.location.href = "home.html"
-  })
-
-
+})
